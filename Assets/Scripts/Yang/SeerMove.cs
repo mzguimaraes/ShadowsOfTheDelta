775ce18163b764a23130322747de0cffb469fb6f
Yang Liu <Yang Liu>
2016-08-03 13:10:42 -0400

    Platformer controls backup

--- Assets/Scripts/Yang/SeerMove.cs
+++ Assets/Scripts/Yang/SeerMove.cs
@@ -6,6 +6,11 @@ using System.Collections;
 public class SeerMove: MonoBehaviour {
 
     public float moveSpeed = 5.0f;
+	public float jumpForce = 5.0f;
+
+	public float raycastLength = 0.1f;
+
+	//public Transform playerFeet;
 
     Vector2 moveVector;
     Rigidbody2D myRigidbody;
@@ -17,22 +22,35 @@ public class SeerMove: MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+
+		SeerJump ();
+
+
+		float horizontal = Input.GetAxis("Horizontal") * moveSpeed;
+        // float vertical = Input.GetAxis("Vertical");
+		float vertical = myRigidbody.velocity.y;
         moveVector = new Vector2(horizontal, vertical);
 
-        // "normalization"
-        //moveVector /= moveVector.magnitude;     // manual normalization
-        //moveVector = moveVector.normalized;     // better way of normalization ("normalized is a property")
-        if (moveVector.magnitude > 1f) {
-            moveVector.Normalize();               // alternate better way of normalization
-        }
+		myRigidbody.velocity = moveVector;
+
+
 	}
 
     void FixedUpdate() {
-        myRigidbody.velocity = moveVector * moveSpeed;
-        // myRigidbody.velocity = new Vector2(0f, 0f);
-        // myRigidbody.velocity = Vector2.zero; // shortcut for the line above
+		
+
+
 
     }
+
+	void SeerJump(){
+		// shoot a short raycast to the ground
+		RaycastHit2D raycast = Physics2D.Raycast (transform.position, -Vector3.up,
+			                       raycastLength);
+		if(raycast.collider != null){
+			if (Input.GetKeyDown (KeyCode.UpArrow)){
+				myRigidbody.AddForce(new Vector2(0f, jumpForce));
+			}
+		}
+	}
 }
