c236d9dd82236ee29236169fe23c5c136183e3bb
Mike Thal <mikeathal@gmail.com>
2016-08-03 13:42:09 -0400

    Edited Door and Key Scripts before merge

--- Assets/Scripts/DoorScript.cs
+++ Assets/Scripts/DoorScript.cs
@@ -8,86 +8,33 @@ public class DoorScript : MonoBehaviour {
 	public Transform Player2Pos;
 
 	// Creating public instance of one key. Left code for a second and third keys.  Uncomment those lines if they're needed.
-	public DoorScript Door1;
-	//public DoorScript Door2;
-	//public DoorScript Door3;
+	public DoorScript Door;
+
 
 
 	// Setting up bools for doors here. Left code for a second and third doors.  Uncomment those lines if they're needed.
-	bool Door1IsUnlocked;
-	// bool Door2IsUnlocked;
-	// bool Door3IsUnlocked;
+	bool DoorIsUnlocked = false;
 
 	// Creating public instance of key so it can be accessed
-	public KeyScript Key1;
-	// public KeyScript Key2;
-	// public KeyScript Key3;
+	public KeyScript Switch;
 
 	public AudioSource DoorSFX;
 	public AudioClip DoorOpening;
 	public AudioClip DoorStillLocked;
 
 	void Start () {
-	
-		// Players haven't unlocked any doors at the beginning of the game.
-		Door1IsUnlocked = false;
-		// Door2IsUnlocked = false;
-		// Door3IsUnlocked = false;
+		
+		Switch.SwitchIsOn = false;
 
 	}
 
 	void Update () {
 
 		// Code that allows Player 1 or Player 2 to Unlock Door 1 after picking up Key 1
-		if (((Door1.transform.position - Player1Pos.position).magnitude < 2.5f) && Input.GetKeyDown (KeyCode.RightShift) && Key1.Player1HasKey1 == true) {
-			DoorSFX.PlayOneShot (DoorOpening);
-			Door1IsUnlocked = true;
-			Debug.Log ("Player 1 Unlocked Door 1");
-
-		} else if (((Door1.transform.position - Player2Pos.position).magnitude < 2.5f) && Input.GetKeyDown (KeyCode.Space) && Key1.Player2HasKey1 == true) {
-
-			DoorSFX.PlayOneShot (DoorOpening);
-			Door1IsUnlocked = true;
-			Debug.Log ("Player 2 Unlocked Door 2");
+		if (Switch.SwitchIsOn == true) {
+			Debug.Log ("Door is Open");
 
 		}
-
-		if (((Door1.transform.position - Player1Pos.position).magnitude < 2.5f) && Input.GetKeyDown (KeyCode.RightShift) && Key1.Player1HasKey1 == false){
-
-			DoorSFX.PlayOneShot (DoorStillLocked);
-			Debug.Log ("Door is still locked");
-		}
-
-
-		// Code that allows Player 1 or Player 2 to unlock Door 2 after picking up Key 2
-		//		if (((Door2.transform.position - Player1Pos.position).magnitude < 2.5f) && Input.GetKeyDown (KeyCode.RightShift) && Player1HasKey2 == true) {
-		//			DoorOpening.Play ();
-		//			Door2IsUnlocked = true;
-		//
-		//		} else if (((Door2.transform.position - Player2Pos.position).magnitude < 2.5f) && Input.GetKeyDown (KeyCode.Space) && Player2HasKey2 == true) {
-		//
-		//			DoorOpening.Play ();
-		//			Door2IsUnlocked = true;
-		//		} else {
-		//
-		//			DoorStillLocked.Play ();
-		//		}
-
-
-		// Code that allows Player 1 or Player 2 to unlock Door 3 after picking up Key 3
-		//		if (((Door3.transform.position - Player1Pos.position).magnitude < 2.5f) && Input.GetKeyDown (KeyCode.RightShift) && Player1HasKey3 == true) {
-		//			DoorOpening.Play ();
-		//			Door3IsUnlocked = true;
-		//
-		//		} else if (((Door3.transform.position - Player2Pos.position).magnitude < 2.5f) && Input.GetKeyDown (KeyCode.Space) && Player2HasKey3 == true) {
-		//
-		//			DoorOpening.Play ();
-		//			Door3IsUnlocked = true;
-		//		} else {
-		//
-		//			DoorStillLocked.Play ();
-		//		}
-
 
 	}
 }
--- Assets/Scripts/KeyScript.cs
+++ Assets/Scripts/KeyScript.cs
@@ -9,40 +9,20 @@ public class KeyScript : MonoBehaviour {
 
 
 	// Creating public instance of one key. Left code for a second and third keys.  Uncomment those lines if they're needed.
-	public KeyScript Key1;
-	// public KeyScript Key2;
-	// public KeyScript Key3;
+	public KeyScript Switch;
 
-
-	// Setting up bools for first player having a key. Left code for a second and third keys.  Uncomment those lines if they're needed.
-	public bool Player1HasKey1;
-	// bool Player1HasKey2;
-	// bool Player1HasKey3;
-
-
-	// Setting up bools for second player having a key. Left code for a second and third keys.  Uncomment those lines if they're needed.
-	public bool Player2HasKey1;
-	// bool Player2HasKey2;
-	// bool Player2HasKey3;
+	public bool SwitchIsOn = false;
 
 
 
 	// Setting up AudioSources to be triggered when players perform certain actions
-	public AudioSource KeySFX;
+	public AudioSource SwitchSFX;
 	public AudioClip KeyPickup;
 
 
 	void Start () {
-
-		// Players don't have any keys at the beginning of the game.
-
-		Player1HasKey1 = false;
-		// Player1HasKey2 = false;
-		// Player1HasKey3 = false;
-
-		Player2HasKey1 = false;
-		// Player2HasKey2 = false;
-		// Player2HasKey3 = false;
+		
+		SwitchIsOn = false;
 
 	}
 	
@@ -50,42 +30,30 @@ public class KeyScript : MonoBehaviour {
 	void Update () {
 
 		// Code that allows Player 1 or Player 2 to pick up Key1.  The input buttons and distance to key are very subject to change.
-		if (((Key1.transform.position - Player1Pos.position).magnitude < 2.5f) && Input.GetKeyDown (KeyCode.RightShift)) {
-			KeySFX.PlayOneShot (KeyPickup);
-			Player1HasKey1 = true;
-			Debug.Log ("Player1HasKey1");
+		if (((Switch.transform.position - Player1Pos.position).magnitude < 2.5f) && Input.GetKeyDown (KeyCode.RightShift) && (SwitchIsOn = false)) {
+			SwitchSFX.PlayOneShot (KeyPickup);
+			SwitchIsOn = true;
+			Debug.Log ("Switch is On");
 
-		} else if (((Key1.transform.position - Player2Pos.position).magnitude < 2.5f) && Input.GetKeyDown (KeyCode.Space)) {
-			KeySFX.PlayOneShot (KeyPickup);
-			Player2HasKey1 = true;
-			Debug.Log ("Player2HasKey1");
-//		} else {
-//			Debug.Log ((Key1.transform.position - Player1Pos.position).magnitude);
+		} else if (((Switch.transform.position - Player1Pos.position).magnitude < 2.5f) && Input.GetKeyDown (KeyCode.RightShift) && (SwitchIsOn = true)) {
+
+			SwitchSFX.PlayOneShot (KeyPickup);
+			SwitchIsOn = false;
+			Debug.Log ("Switch is Off");
 		}
 
 
-		// Code that allows Player 1 or Player 2 to pick up Key2.  The input buttons and distance to key are very subject to change. Uncomment if neeeded.
-//		if (((Key2.transform.position - Player1Pos.position).magnitude < 2.5f) && Input.GetKeyDown (KeyCode.RightShift)) {
-//			KeyPickUp.Play ();
-//			Player1HasKey2 = true;
+		if (((Switch.transform.position - Player2Pos.position).magnitude < 2.5f) && Input.GetKeyDown (KeyCode.E) && (SwitchIsOn = false)) {
 
-//		} else if (((Key2.transform.position - Player2Pos.position).magnitude < 2.5f) && Input.GetKeyDown (KeyCode.Space)) {
-//			KeyPickUp.Play ();
-//			Player2HasKey2 = true;
-//		}
+			SwitchSFX.PlayOneShot (KeyPickup);
+			SwitchIsOn = true;
+			Debug.Log ("Switch is On");
 
+		} else if (((Switch.transform.position - Player2Pos.position).magnitude < 2.5f) && Input.GetKeyDown (KeyCode.E) && (SwitchIsOn = true)) {
 
-		// Code that allows Player 1 or Player 2 to pick up Key3.  The input buttons and distance to key are very subject to change. Uncomment if neeeded.
-//		if (((Key3.transform.position - Player1Pos.position).magnitude < 2.5f) && Input.GetKeyDown (KeyCode.RightShift)) {
-//			KeyPickUp.Play ();
-//			Player1HasKey3 = true;
-
-//		} else if (((Key3.transform.position - Player2Pos.position).magnitude < 2.5f) && Input.GetKeyDown (KeyCode.Space)) {
-//			KeyPickUp.Play ();
-//			Player2HasKey3 = true;
-//		}
-
-
-
+			SwitchSFX.PlayOneShot (KeyPickup);
+			SwitchIsOn = false;
+			Debug.Log ("Switch is Off");
+		}
 	}
 }
