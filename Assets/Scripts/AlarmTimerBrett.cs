b98843ccd60a88790ad20e8af371122270d872b0
Brett Moody <brettmoody@me.com>
2016-08-04 16:44:51 -0400

    Implemented basic alarm / timer system for levels. Created Lose Screen for when the timer ran out.

--- Assets/Scripts/AlarmTimerBrett.cs
+++ Assets/Scripts/AlarmTimerBrett.cs
@@ -1,15 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class AlarmTimerBrett : MonoBehaviour {
 
+
+	public Text timeRemainingText;
+	public float timeLeft;
+
+
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+
+		if (timeLeft > 0) {
+			timeLeft -= Time.deltaTime;
+			timeRemainingText.text = (int)timeLeft + " seconds left.";
+		}
+
+		if (timeLeft <= 0) {
+			SceneManager.LoadScene("LoseScreenBrett");
+
+		}
+
 	}
 }
