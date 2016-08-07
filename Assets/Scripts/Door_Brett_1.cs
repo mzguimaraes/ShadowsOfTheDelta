e15417a142c9668fd96be1c36ac751b5c1d2bd62
Brett Moody <brettmoody@me.com>
2016-08-06 20:41:24 -0400

    Levers now change colors when you click on them.

--- Assets/Scripts/Door_Brett_1.cs
+++ Assets/Scripts/Door_Brett_1.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Door : MonoBehaviour {
+public class Door_Brett_1: MonoBehaviour {
 
 	// Creating public transform variables for both players. These will be used for the key pickup and door unlock codes.
 	public Transform Player1Pos;  
@@ -13,10 +13,11 @@ public class Door : MonoBehaviour {
 	Collider2D DoorCollider;
 
 	// Setting up bools for doors here. Left code for a second and third doors.  Uncomment those lines if they're needed.
-	bool DoorIsUnlocked = false;
+	// bool DoorIsUnlocked = false;
 
 	// Creating public instance of key so it can be accessed
-	public SwitchFunctions Switch;
+	public SwitchFunctionsBrett Switch;
+	public SwitchFunctionsBrett Switch2;
 
 	public AudioSource DoorSFX;
 	public AudioClip DoorOpening;
@@ -33,15 +34,18 @@ public class Door : MonoBehaviour {
 	void Update () {
 
 		// Code that allows Player 1 or Player 2 to Unlock Door 1 after picking up Key 1
-		if (Switch.SwitchIsOn == true) {
+		if (Switch.SwitchIsOn == true && Switch2.SwitchIsOn == true) {
 
 			DoorSprite.enabled = false;
 			DoorCollider.enabled = false;
 
-		} else if (Switch.SwitchIsOn == false) {
+
+
+		} else if (Switch.SwitchIsOn == false && Switch2.SwitchIsOn == false) {
 
 			DoorSprite.enabled = true;
 			DoorCollider.enabled = true;
+
 
 		}
 
---
+++ Assets/Scripts/SwitchFunctionsBrett.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SwitchFunctionsBrett : MonoBehaviour {
+
+	// Creating public transform variables for both players. 
+	public Transform Player1Pos;
+	public Transform Player2Pos;
+
+	SpriteRenderer DoorSprite; // This makes the sprite renderer for this lever available.
+	public Sprite blueLever; // This is the sprite that represents the ON position.
+	public Sprite redLever; // This is the sprite that represents the OFF position.
+
+	public bool SwitchIsOn = false; // Setting the switch to false intially. Doors are THERE when switches are OFF.
+	public SwitchFunctionsBrett otherSwitch;  // Getting access to the switch's twin.
+
+	public Text playerOneInstruction; // These text boxes tell the player which buttons to press.
+	public Text playerTwoInstruction;
+	public GameObject playerOnePanel; // These panels contain the players' intstructions.
+	public GameObject playerTwoPanel;
+
+	// Setting up AudioSources to be triggered when players perform certain actions
+	public AudioSource SwitchSFX;
+	public AudioClip KeyPickup;
+
+	public bool switchColor;
+
+
+	void Start () {
+		SwitchIsOn = false; //I believe this is redundant.
+		DoorSprite = GetComponent<SpriteRenderer> ();
+	}
+
+
+	void FlipSwitch () {
+		SwitchSFX.PlayOneShot (KeyPickup);
+		SwitchIsOn = !SwitchIsOn;
+		otherSwitch.SwitchIsOn = SwitchIsOn;
+		switchColor = true;
+		otherSwitch.switchColor = true;
+//		
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+			
+
+		if (switchColor) {
+			if (SwitchIsOn == true) {
+				DoorSprite.sprite = blueLever;
+			} else if (SwitchIsOn == false) {
+				DoorSprite.sprite = redLever;
+			}
+			switchColor = false;
+		}
+		
+		
+
+		// Code that allows Player 1 to flip the switch.
+		if ((transform.position - Player1Pos.position).magnitude < 2.5f) {
+			playerOnePanel.SetActive (true);
+			playerOneInstruction.text = "Press RIGHT SHIFT to flip the switch!";
+			if (Input.GetKeyDown (KeyCode.RightShift)) {
+				FlipSwitch();
+			// Debug.Log (SwitchIsOn);
+			}
+
+		} 
+//			else if (((transform.position - Player1Pos.position).magnitude < 2.5f) && Input.GetKeyDown (KeyCode.RightShift) && (SwitchIsOn == true)) {
+////
+////			SwitchSFX.PlayOneShot (KeyPickup);
+////			SwitchIsOn = false;
+////			Debug.Log (SwitchIsOn);
+////		}
+
+
+		if (((transform.position - Player2Pos.position).magnitude < 2.5f)) {
+
+			playerTwoPanel.SetActive (true);
+			playerTwoInstruction.text = "Press SPACE to flip the switch!";
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				FlipSwitch ();
+			}
+
+		}
+//			else if (((transform.position - Player2Pos.position).magnitude < 2.5f) && Input.GetKeyDown (KeyCode.Space) && (SwitchIsOn = true)) {
+//
+//			SwitchSFX.PlayOneShot (KeyPickup);
+//			SwitchIsOn = false;
+//			Debug.Log ("Switch is Off");
+//		}
+	}
+}
