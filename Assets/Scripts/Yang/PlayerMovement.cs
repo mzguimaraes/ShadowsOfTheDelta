d2b942fe3656224b02444674a38a57de8f04e6cd
Yang Liu <lykavin@hotmail.com>
2016-08-09 17:21:12 -0400

    Added prisoner sprites. Changed PlayerMovement script to let the player face where she is heading for. Changed player sprites in Yang_PlayerControlTest_topdown scene.

--- Assets/Scripts/Yang/PlayerMovement.cs
+++ Assets/Scripts/Yang/PlayerMovement.cs
@@ -10,18 +10,27 @@ public class PlayerMovement: MonoBehaviour {
 	public string horizontalAxisName = "Horizontal";
 	public string verticalAxisName = "Vertical";
 
+	public enum Direction {NORTH, SOUTH, EAST, WEST};
+
+	public Direction initialFacing = Direction.SOUTH;
 	//public Transform playerFeet;
 
     Vector2 moveVector;
     Rigidbody2D myRigidbody;
 
+	Vector2 lastDirection;
+
 	// Use this for initialization
 	void Start () {
         myRigidbody = GetComponent<Rigidbody2D>();
+
+		lastDirection = DirectionToVector(initialFacing);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		// get the axis values, construct a vector and normalize it
 		float horizontal = Input.GetAxis (horizontalAxisName) * moveSpeed;
 		float vertical = Input.GetAxis(verticalAxisName) * moveSpeed;
         moveVector = new Vector2(horizontal, vertical);
@@ -30,10 +39,34 @@ public class PlayerMovement: MonoBehaviour {
 		}
 
 
+
+
 	}
 
     void FixedUpdate() {
 		myRigidbody.velocity = moveVector * moveSpeed * Time.deltaTime * 10f;
+
+		if(moveVector.magnitude != 0f){
+			transform.up = moveVector.normalized;
+			lastDirection = moveVector.normalized;
+		}else{
+			transform.up = lastDirection;
+		}
+
     }
 
+	// translate a Direction enum to a normalized Vector3
+	Vector3 DirectionToVector(Direction dir){
+		if(dir == Direction.NORTH){
+			return new Vector3 (0f, 1f, 0f);
+		}else if(dir == Direction.SOUTH){
+			return new Vector3 (0f, -1f, 0f);
+		}else if(dir == Direction.EAST){
+			return new Vector3 (1f, 0f, 0f);
+		}else if (dir == Direction.WEST){
+			return new Vector3 (-1f, 0f, 0f);
+		}else{
+			return new Vector3 (0f, 0f, 0f);
+		}
+	}
 }
