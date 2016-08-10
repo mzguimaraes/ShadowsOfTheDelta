d66f195a1e5b388dfaddd8fc2dcd05c521b5a699
Brett Moody <brettmoody@me.com>
2016-08-10 12:36:09 -0400

    Updated help screen and made various bug fixes.

---
+++ Assets/Scripts/ControlScreenStart.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ControlScreenStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKeyDown (KeyCode.J)) {
+			SceneManager.LoadScene (2);
+			// Debug.Log (SwitchIsOn);
+		}
+
+	}
+}
